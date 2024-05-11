19927f84811b84a210030ef67b3e251af47e1d1c
Dorian11ro-coding <minimickey427@yahoo.com>
2024-05-11 13:26:06 +0300

    First release

---
+++ Calculator.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    cout << "For additions, press 1\n";
+    cout << "For subtractions, press 2\n";
+    cout << "For multiplications, press 3\n";
+    cout << "For divisions, press 4\n";
+    cout << "For powers, press 5\n";
+    cout << "For finding the area of a circle, press 6\n";
+    cout << "For finding the square root, press 7\n";
+
+    cout << "Choice: ";
+    int choice;
+    string strchoice;
+
+    cin >> choice;
+    float n1;
+    float n2;
+    float exponent;
+    float baza;
+    float radius;
+    float pi = 3.14159;
+    double sqrtnmb;
+    switch(choice){
+            case 1:
+                cout << "Number 1: ";
+                cin >> n1;
+                cout << "Number 2: ";
+                cin >> n2;
+                cout << n1+n2 << "\n";
+                return 0;
+
+            case 2:
+                cout << "Number 1: ";
+                cin >> n1;
+                cout << "Number 2: ";
+                cin >> n2;
+                cout << n1-n2 << "\n";
+                return 0;
+
+            case 3:
+                cout << "Number 1: ";
+                cin >> n1;
+                cout << "Number 2: ";
+                cin >> n2;
+                cout << n1*n2 << "\n";
+                return 0;
+
+            case 4:
+                cout << "Number 1: ";
+                cin >> n1;
+                cout << "Number 2: ";
+                cin >> n2;
+                cout << n1/n2 << "\n";
+                return 0;
+
+            case 5:
+                cout << "Base: ";
+                cin >> baza;
+                cout << "Exponent: ";
+                cin >> exponent;
+                cout << pow(baza, exponent) << "\n";
+                return 0;
+
+            case 6:
+                cout << "Radius: ";
+                cin >> radius;
+                cout << "Area of circle is " << pi*radius*radius << "\n";
+                return 0;
+
+            case 7:
+                cout << "Enter a number: ";
+                cin >> sqrtnmb;
+                if(sqrtnmb < 0){
+                    cout << "Square root negative numbers are undefined\n";
+                    return 1;
+                }
+                else{
+                    cout << "Square root of " << sqrtnmb << " is: " << sqrt(sqrtnmb) << "\n";
+                    return 0;
+                }
+                return 0;
+    }
+}
